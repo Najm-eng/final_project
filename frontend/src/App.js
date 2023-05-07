38214b833dae784f664a023fe5d463be3f47d35e
najm <n0jm202@gmail.com>
2023-05-07 12:01:07 -0400

    Creating-React-App

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">amazona</a> {/* This is a link */}
+      </header>
+      <main>list of prodects</main>
+    </div>
+  );
+}
+
+export default App;
